d995aa91c46d1939dc9fd7e8c55434ca4d885294
Baghdad AlRuwaili <bgdad.65@gmail.com>
2025-11-16 02:52:48 +0300

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author baghd
+ */
+import java.util.Scanner;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int num1 = input.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = input.nextInt();
+
+        int sum = num1 + num2;
+
+        System.out.println("The sum is: " + sum);
+    }
+}
+
